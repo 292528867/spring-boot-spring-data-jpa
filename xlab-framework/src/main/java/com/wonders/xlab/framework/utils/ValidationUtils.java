c38c88579e3276e7b8efd91ec49257e26a5fcc33
王强 <wangqinag02@wondersgroup.com>
2015-10-09 22:31:25 +0800

    add ValidationUtils

---
+++ xlab-framework/src/main/java/com/wonders/xlab/framework/utils/ValidationUtils.java
@@ -0,0 +1,17 @@
+package com.wonders.xlab.framework.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by wangqiang on 15/10/9.
+ */
+public class ValidationUtils {
+
+    public static boolean isMobiles(String mobiles) {
+        Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
+        Matcher m = p.matcher(mobiles);
+        return m.matches();
+    }
+
+}
